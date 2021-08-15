eb66c790ca234194bc093734ade7050f678a95a4
Bijan Regmi <vizandabijan@gmail.com>
2021-08-16 02:02:22 +0545

    Changed inactive window color

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -25,9 +25,10 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_bord[]		= "#badee3";
+static const char col_blck[]		= "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_bord },
+	[SchemeNorm] = { col_gray3, col_gray1, col_blck },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_bord  },
 };
 
