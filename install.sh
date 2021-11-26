902ad5c483950c01cef81603c2f7606b111d5dc9
Bijan Regmi <vizandabijan@gmail.com>
2021-11-26 22:40:59 +0545

    newterm st patch

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@ red="\e[0;91m"
 yellow="\e[0;33m"
 reset="\e[0m"
 
-BASEDIR=$(dirname "$0")
-cd $BASEDIR
+BASEFULLDIR=$(realpath $(dirname "$0"))
+TMPDIR=$BASEFULLDIR/tmp
+
+cd $BASEFULLDIR
+mkdir -p tmp
+cd $TMPDIR
 
 function create_links(){
     echo -e "\n$green[+] Creating links$reset"
@@ -20,9 +24,13 @@ function create_links(){
     ln -s $1 $2
 }
 
+function cleanup(){
+    cd $BASEFULLDIR
+    rm -r tmp
+}
+
 function st_install(){
-    mkdir -p tmp
-    cd tmp
+    cd $TMPDIR
 
     echo -e "\n$green[+] Downloading st version 0.8.4$reset"
     wget https://dl.suckless.org/st/st-0.8.4.tar.gz
@@ -38,23 +46,20 @@ function st_install(){
     patch < ../../st/alpha.diff
     echo -e "\n$yellow[3] Enable del key$reset"
     patch < ../../st/delkey.diff
-    echo -e "\n$yellow[4] Personal preferences$reset"
-    patch < ../../st/personalConfigs.diff
+    echo -e "\n$yellow[4] NewTerm in cwd$reset"
+    patch < ../../st/newterm.diff
 
     echo -e "\n$green[+] Compiling$reset"
-    cp config.def.h config.h
+    cp $BASEFULLDIR/st/config.h config.h
     sudo make install
-
-    echo -e "\n$green[+] Cleaning Up$reset"
-    cd ../../
-    sudo rm -r tmp
+    
+    cd ..
 
     echo -e "\n$green[+] Installation of st complete :)$reset"
 }
 
 function dwm_install(){
-    mkdir -p tmp
-    cd tmp
+    cd $TMPDIR
 
     echo -e "\n$green[+] Downloading dwm version 6.2$reset"
     wget https://dl.suckless.org/dwm/dwm-6.2.tar.gz
@@ -73,8 +78,7 @@ function dwm_install(){
     cd ../../
     create_links $(pwd)/dwm/autostart.sh /home/$USER/.dwm/autostart.sh
 
-    echo -e "\n$green[+] Cleaning Up$reset"
-    sudo rm -r tmp
+    cd $TMPDIR
 
     echo -e "\n$green[+] Installation of dwm complete :)$reset"
 }
@@ -83,7 +87,7 @@ function rofi_install(){
     echo -e "\n$green[+] Installing rofi$reset"
     sudo pacman -S rofi
 
-    create_links $(pwd)/rofi /home/$USER/.config/rofi
+    create_links $BASEFULLDIR/rofi /home/$USER/.config/rofi
 
     echo -e "\n$green[+] Installation of rofi complete :)$reset"
 }
@@ -92,7 +96,7 @@ function ranger_install(){
     echo -e "\n$green[+] Installing ranger$reset"
     sudo pacman -S ranger
 
-    create_links $(pwd)/ranger /home/$USER/.config/ranger
+    create_links $BASEFULLDIR/ranger /home/$USER/.config/ranger
     echo -e "\n$green[+] Installation of ranger complete :)$reset"
 }
 
@@ -101,7 +105,7 @@ function nvim_install(){
     sudo pacman -S neovim
 
     echo -e "\n$green[+] Working on Plugins:$reset"
-    create_links $(pwd)/nvim /home/$USER/.config/nvim
+    create_links $BASEFULLDIR/nvim /home/$USER/.config/nvim
 
     echo -e "\n$green[+] Installation of neovim complete :)$reset"
 }
@@ -110,7 +114,7 @@ function neofetch_install(){
     echo -e "\n$green[+] Installing neofetch$reset"
     sudo pacman -S neofetch
 
-    create_links $(pwd)/neofetch /home/$USER/.config/neofetch
+    create_links $BASEFULLDIR/neofetch /home/$USER/.config/neofetch
 
     echo -e "\n$green[+] Installation of neofetch complete :)$reset"
 }
@@ -132,8 +136,8 @@ menu() {
     echo -ne "$prompt"
 }
 
-options=("st" "dwm" "rofi" "ranger" "neovim" "neofetch")
-install_fxn=(st_install dwm_install rofi_install ranger_install nvim_install neofetch_install)
+options=("st" "dwm" "rofi" "ranger" "neovim" "neofetch" "cleanup")
+install_fxn=(st_install dwm_install rofi_install ranger_install nvim_install neofetch_install cleanup)
 
 prompt="Select components to install. Input 0 once done selecting: "
 menu
@@ -151,5 +155,5 @@ while read ch; do
 done
 
 for i in ${!options[@]}; do 
-    [[ "${selections[i]}" == "*" ]] && { printf "\n$green[#] Installing %s$reset\n" "${options[i]}";} && ${install_fxn[i]}
+    [[ "${selections[i]}" == "*" ]] && { printf "\n$green[#] Executing option: %s$reset\n" "${options[i]}";} && ${install_fxn[i]}
 done
