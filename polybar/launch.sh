3960320fc2bb84fd16955e57abbff8813be18611
Bijan Regmi <vizandabijan@gmail.com>
2021-11-29 22:13:47 +0545

    new polybar theme

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,4 +9,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 for i in /sys/class/hwmon/hwmon*/temp*_input; do if [ "$(cat ${i%_*}_label 2>/dev/null)" = "Core 0" ]; then echo $(readlink -f $i) > $DIR/temp_location.ini; fi done
 
 polybar -q bar-eDP1 -c "$DIR"/config.ini &
-polybar -q bar-HDMI -c "$DIR"/config.ini &
+# polybar -q bar-HDMI -c "$DIR"/config.ini &
