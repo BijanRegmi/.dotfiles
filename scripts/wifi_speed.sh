ee2c55546a93070178d775a9314b1c40bf1a9b3a
Bijan Regmi <vizandabijan@gmail.com>
2021-12-30 14:34:15 +0545

    bar click commands

--- scripts/wifi_speed.sh
+++ scripts/wifi_speed.sh
@@ -5,7 +5,7 @@ kilo=$(echo "($curr - $prev)/1024" | bc)
 echo $curr > /tmp/rx
 
 if [ $kilo  -gt 1024 ]; then
-	echo "$(echo "scale = 1; $kilo / 1024" | bc) MB/s"
+	echo "%{A1:st -e bash -c 'speedtest; read;':}$(echo "scale = 1; $kilo / 1024" | bc) MB/s%{A}"
 else
-	echo "$kilo KB/s"
+	echo "%{A1:st -e bash -c 'speedtest; read;':}$kilo KB/s%{A}"
 fi
--- scripts/wifi_state.sh
+++ scripts/wifi_state.sh
@@ -1,6 +1,6 @@
 state=$(iwctl station wlan0 show | grep State | awk '{ print $2 }')
 if [ $state == "connected" ]; then
-	echo "%{F#00ff00}%{F-}"
+	echo "%{A1:st -e bash -c 'iwctl station wlan0 show; read;':}%{F#00ff00}%{F-}%{A}"
 else
-    echo "%{F#ff0000}%{F-}"
+    echo "%{A1:st -e bash -c 'iwctl station wlan0 scan; sleep 1; iwctl station wlan0 get-networks; read a; iwctl station wlan0 connect $a; bash;':}%{F#ff0000}%{F-}%{A}"
 fi
