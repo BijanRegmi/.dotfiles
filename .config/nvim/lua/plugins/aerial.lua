5cbf290e7167c742a221871b4d982f5806d11c75
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2024-01-28 21:12:23 +0545

    aerial

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -7,6 +7,7 @@ return {
     init = function()
         vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>", keyopts("Toggle Aerial"))
     end,
+    config = true,
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
         "nvim-tree/nvim-web-devicons",
--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -2,5 +2,5 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     event = "BufRead",
-    opts = { enabled = true, scope = { enabled = true } },
+    opts = {},
 }
