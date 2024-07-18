9bf3a58ec8eb4a23c63ce1b992af66a494bb87c4
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2024-07-18 21:33:41 +0545

    add cliphist

--- .config/hypr/scripts/startup.sh
+++ .config/hypr/scripts/startup.sh
@@ -13,3 +13,8 @@ waybar &
 dunst &
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1&
+
+wl-paste --type text --watch cliphist store& #Stores only text data
+
+wl-paste --type image --watch cliphist store& #Stores only image data
+
